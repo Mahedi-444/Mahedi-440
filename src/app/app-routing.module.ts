28be9e91b4d17542fe8e820e832974fa559de30c
Mahedi-444 <mahedialilodha280@gmail.com>
2022-03-26 15:54:26 +0530

    deploy angular app to heroku demo code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { FormComponent } from './form/form.component';
+import { FormtwoComponent } from './formtwo/formtwo.component';
+import { PhoneLoginComponent } from './phone-login/phone-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"",
+    component:PhoneLoginComponent,
+  },
+  {
+    path:"phone-login",
+    component:PhoneLoginComponent
+  },
+  {
+    path:"form",canActivate:[AuthGuard],
+    component:FormComponent,
+  },
+  
+  {
+    path:"formtwo",canActivate:[AuthGuard],
+    component:FormtwoComponent,
+  },
+
+  // {
+  //   path:'form',
+  //   component:FormComponent,
+  // },
+ 
+  
+
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
